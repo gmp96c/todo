fd722ed057cf887725d7c75a88113eb670699c9d
gpm96c <griffinpotts96@gmail.com>
2020-03-01 13:11:42 -0800

    Initial commit

---
+++ components/Task.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const TaskStyle = styled.div`
+background: red;
+`;
+
+export const Task = props => {
+  return (
+    <TaskStyle>
+
+    </TaskStyle>
+  )
+}
